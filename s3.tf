c408b8b0966981b0cf3aa5232cd2758532546843
pramod413 <89720793+pramod413@users.noreply.github.com>
2022-05-17 19:52:26 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,1 +1,13 @@
-crate s3
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = "my-bucket"
+  acl    = "private"
+
+
+ tags = {
+    Environment = "Dev"
+  }
+}
